2d3d902c303806b51ad34146e2f149bd4580ea2d
duhaalazzam <dohabalazzam@gmail.com>
2021-01-27 14:00:13 +0200

    add javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+var numOfMatches=prompt("How many Fasily matches you have seen?")
+if(numOfMatches>=20)
+{
+    document.write("Hi our good follower")
+}else if(numOfMatches>=10)
+{
+    document.write("Hi our follower,You shall see more of our matches")
+}else if(numOfMatches<10)
+{
+    document.write("Hi our good follower,You didnt see a lot of our matches")
+}
