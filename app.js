6f2088e9eab9ef64094b81c265a62f8f7cc917e5
duhaalazzam <dohabalazzam@gmail.com>
2021-01-28 13:04:09 +0200

    add while

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var numOfMatches=prompt("How many Fasily matches you have seen?")
+/*var numOfMatches=prompt("How many Fasily matches you have seen?")
 if(numOfMatches>=20)
 {
     document.write("Hi our good follower")
@@ -8,4 +8,14 @@ if(numOfMatches>=20)
 }else if(numOfMatches<10)
 {
     document.write("Hi our follower,You didnt see a lot of our matches")
+}*/
+var star=prompt("What star rating would you give, 1-5?")
+while(star<1||star>5)
+{
+    alert("Invalid input")
+    star=prompt("What star rating would you give, 1-5?")
+}
+for(var i=0;i<star;i++)
+{
+document.write(<img src=fifa.jpg />)
 }
