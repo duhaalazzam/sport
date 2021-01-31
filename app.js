cac7d6d16a185faaec6e5a1205627b61b9a25241
duhaalazzam <dohabalazzam@gmail.com>
2021-01-31 13:55:21 +0200

    add 2 dynamic component

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-/*var numOfMatches=prompt("How many Fasily matches you have seen?")
+var hourNow = prompt('what is the time now');
+while(hourNow==''||hourNow>24||hourNow<0){
+    hourNow = prompt('what is the time now');
+}
+var message= '';
+if(hourNow >= 18  && hourNow <= 24){ //f
+    message = 'Good Evening';
+}else if(hourNow >= 12 && hourNow < 18){
+    message = 'Good Afternoon';
+}else if (hourNow >= 0 && hourNow< 12){
+    message = 'Good morning';
+}
+document.write('<h2>' + message + '</h2>');
+
+var numOfMatches=prompt("How many Fasily matches you have seen?")
+while(numOfMatches=='')
+{
+    numOfMatches=prompt("How many Fasily matches you have seen?") 
+}
 if(numOfMatches>=20)
 {
     document.write("Hi our good follower")
@@ -8,7 +26,7 @@ if(numOfMatches>=20)
 }else if(numOfMatches<10)
 {
     document.write("Hi our follower,You didnt see a lot of our matches")
-}*/
+}
 var star=prompt("What star rating would you give, 1-5?")
 while(!(star>=1 && star<=5))
 {
